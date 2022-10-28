46a0cc77fe15857ff8a5f164c504716e62a7a580
Rafael <rafaelnakati@igui.com>
2022-10-28 11:16:17 -0300

    others components of design system

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,13 @@ import './styles/global.css';
 
 export function App() {
   return (
-    <h1>Hello World!</h1>
+    <>
+      <h1 className='font-bold text-2xl text-violet-500'>Hello World!</h1>
+
+      <button className='bg-cyan-500'>
+        Enviar
+      </button>
+    </>
+    
   )
 }
